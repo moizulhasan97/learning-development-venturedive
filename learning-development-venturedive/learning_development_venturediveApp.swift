7df2e870193ac3a2ff139016b79f9f653efbec80
Moiz Ul Hasan <moiz.hasan@ext.beyond.one>
2025-12-18 20:05:25 +0500

    initial commit

---
+++ learning-development-venturedive/learning_development_venturediveApp.swift
@@ -0,0 +1,17 @@
+//
+//  learning_development_venturediveApp.swift
+//  learning-development-venturedive
+//
+//  Created by Moiz Ul Hasan on 18/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct learning_development_venturediveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
